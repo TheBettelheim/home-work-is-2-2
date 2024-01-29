2145c3986ccae5d6490e5055012116e0bd8e463a
TheBettelheim <evdakdurak@gmail.com>
2024-01-29 15:33:58 +0300

    Написал программу на многомерный массив

--- task_2/main.js
+++ task_2/main.js
@@ -1,10 +1,2 @@
-let temperature = prompt("Введите текущую температуру:");
-if (temperature >30) {
-    console.log("Вы можете надеть легкую одежду")
-} else if (temperature >20) {
-    console.log("Нужно надеть теплую одежду")
-} else if (temperature <=20) {
-    console.log("Сегодня холодно и нужно надеть зимнюю одежду")
-} else {
-    console.log("Температура в пределах нормы.")
-}
+let studentsandteachers = [["Иван Р", "Степан П", "Станислав Г"],["Оксана С", "Татьяна В"]]
+console.log(studentsandteachers)
